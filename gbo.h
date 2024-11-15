b21c18e8fb33cbf2b589885ac2e3fbdfe87f4258
BioGD <146005895+BioGD@users.noreply.github.com>
2024-11-15 11:59:23 +0300

    Create gbo.h

---
+++ gbo.h
@@ -0,0 +1,6 @@
+#ifndef GBO_H
+#define GBO_H
+
+extern "C" void x_next(double** population, int cur_vec, int best_ind, int worst_ind, unsigned char cur_iter, unsigned char iter_number, double pr, int th, int n);
+
+#endif
