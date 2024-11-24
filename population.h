f2d781cfd9708aa40e58da99a3f689bbec25affc
BioGD <146005895+BioGD@users.noreply.github.com>
2024-11-25 01:36:58 +0300

    Update population.h

--- population.h
+++ population.h
@@ -7,5 +7,8 @@ extern "C" double** create_population(int th, int d);
 extern "C" double get_s0_sum(double** block);
 extern "C" double get_s1_sum(double** block);
 extern "C" double of(double** original_block, double** changed_block, char b);
+extern "C" int find_x_best(double** population, double** original_dct_block, int popul_size, const char b);
+extern "C" int find_x_worst(double** population, double** original_dct_block, int popul_size, const char b);
+extern "C" double** apply_x(double** block, double* x);
 
 #endif // POPULATION_H
