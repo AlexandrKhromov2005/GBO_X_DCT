27c180a95c57af13f739ca81cd3cf51beacbbff2
BioGD <146005895+BioGD@users.noreply.github.com>
2024-11-29 17:17:53 +0300

    Update block_metrics.c

--- block_metrics.c
+++ block_metrics.c
@@ -1,6 +1,6 @@
 #include <math.h>
 
-double block_mse(unsigned char** original_block, unsigned char** changed_block) {
+double block_mse(unsigned char original_block[8][8], unsigned char changed_block[8][8]) {
     double mse = 0.0;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
@@ -11,6 +11,11 @@ double block_mse(unsigned char** original_block, unsigned char** changed_block) 
     return mse / 64.0;
 }
 
-double block_psnr(unsigned char** original_block, unsigned char** changed_block) {
-    return 10 * log10((255.0 * 255.0) / block_mse(original_block, changed_block));
+double block_psnr(unsigned char original_block[8][8], unsigned char** changed_block[8][8]) {
+    double mse = block_mse(original_block, changed_block);
+    if (mse == 0) {
+        mse = 0.00001;
+    }
+
+    return 10 * log10((255.0 * 255.0) / mse);
 }
