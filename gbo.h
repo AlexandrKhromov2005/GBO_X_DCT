ced84b258f20ab2d814e5a316d170c425f1e2147
BioGD <146005895+BioGD@users.noreply.github.com>
2024-11-29 17:22:15 +0300

    Update gbo.h

--- gbo.h
+++ gbo.h
@@ -1,6 +1,6 @@
 #ifndef GBO_H
 #define GBO_H
 
-extern "C" void gbo(double** population, int best_ind, int worst_ind, int cur_inter, int m, int n, double pr, double th);
+extern "C" void gbo(double population[][22], int best_ind, int worst_ind, int cur_iter, int m, int n, double pr, double th);
 
 #endif
