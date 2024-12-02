44616bdea040d04969386898f0fae6ed88815b11
AlexandrKhromov2005 <146848229+AlexandrKhromov2005@users.noreply.github.com>
2024-12-02 11:58:55 +0300

    Update gbo.c
    rewrite func to macros

--- gbo.c
+++ gbo.c
@@ -6,44 +6,34 @@
 #include "population.h"
 
 #define POPSIZE 100 
-
 #define PI acos(-1)
-
-//Вычисление rand от 0 до 1
-double rand_num() {
-	return (double)rand() / RAND_MAX;
-}
-
-//Вычисление  нормально распределенного числа от 0 до 1
-double randn() {
-	static int use_last = 0;
-	static double y2;
-
-	if (use_last) {
-		use_last = 0;
-		return (y2 + 3) / 6.0;
-	}
-
-	double x1, x2, w, y1;
-
-	do {
-		x1 = 2.0 * rand_num() - 1.0;
-		x2 = 2.0 * rand_num() - 1.0;
-		w = x1 * x1 + x2 * x2;
-	} while (w >= 1.0 || w == 0.0);
-
-	w = sqrt((-2.0 * log(w)) / w);
-	y1 = x1 * w;
-	y2 = x2 * w;
-
-	use_last = 1;
-
-	return (y1 + 3) / 6.0;
-}
+#define RAND_DOUBLE()((double)rand() / RAND_MAX) 
+#define RANDN() \
+({ \
+    static int use_last = 0; \
+    static double y2; \
+    double x1, x2, w, y1, result; \
+    if (use_last) { \
+        use_last = 0; \
+        result = (y2 + 3) / 6.0; \
+    } else { \
+        do { \
+            x1 = 2.0 * (RAND_DOUBLE()) - 1.0; \
+            x2 = 2.0 * (RAND_DOUBLE()) - 1.0; \
+            w = x1 * x1 + x2 * x2; \
+        } while (w >= 1.0 || w == 0.0); \
+        w = sqrt((-2.0 * log(w)) / w); \
+        y1 = x1 * w; \
+        y2 = x2 * w; \
+        use_last = 1; \
+        result = (y1 + 3) / 6.0; \
+    } \
+    result; \
+})
 
 //Вычисление rho
 double new_rho(double alpha) {
-	return (2 * rand_num() * alpha) - alpha;
+	return (2 * (RAND_DOUBLE()) * alpha) - alpha;
 }
 
 //Вычисление инедксов рандомных векторов
@@ -80,8 +70,8 @@ void gsr(double x_next[22], double x[22], double x1[22], double x2[22], double x
 
 	double rho1 = new_rho(*alpha); //Инициализация rho1
 	double rho2 = new_rho(*alpha); //Инициализация rho2
-	double ra = rand_num();		   //Инициализация ra	
-	double rb = rand_num();        //Инициализация rb	
+	double ra = (RAND_DOUBLE());	//Инициализация ra	
+	double rb = (RAND_DOUBLE());	//Инициализация rb	
 	
 	double x3[22];
 
@@ -108,21 +98,21 @@ void gsr(double x_next[22], double x[22], double x1[22], double x2[22], double x
 
 	//Вычисление вектора следующей популяции
 	for (int i = 0; i < 22; i++) {
-		delta[i] = 2.0 * rand_num() * fabs(((xr1[i] + xr2[i] + xr3[i] + xr4[i]) / 4.0) - x[i]);
+		delta[i] = 2.0 * (RAND_DOUBLE()) * fabs(((xr1[i] + xr2[i] + xr3[i] + xr4[i]) / 4.0) - x[i]);
 
 		step[i] = ((x_best[i] - xr1[i]) + delta[i]) / 2.0;
 
 		x_delta[i] = ((double)(rand() % n) + 1) * fabs(step[i]);
 
-		z[i] = x[i] - randn() * ((2 * x_delta[i] * x[i]) / (x_worst[i] - x_best[i] + epsilon));
+		z[i] = x[i] - (RANDN()) * ((2 * x_delta[i] * x[i]) / (x_worst[i] - x_best[i] + epsilon));
 
-		p[i] = rand_num() * (((z[i] + x[i]) / 2.0) + rand_num() * x_delta[i]);
+		p[i] = (RAND_DOUBLE()) * (((z[i] + x[i]) / 2.0) + (RAND_DOUBLE()) * x_delta[i]);
 
-		q[i] = rand_num() * (((z[i] + x[i]) / 2.0) - rand_num() * x_delta[i]);
+		q[i] = (RAND_DOUBLE()) * (((z[i] + x[i]) / 2.0) - (RAND_DOUBLE()) * x_delta[i]);
 
-		x1[i] = x[i] - randn() * rho1 * ((2 * x_delta[i] * x[i]) / (p[i] - q[i] + epsilon)) + rand_num() * rho2 * (x_best[i] - x[i]);
+		x1[i] = x[i] - (RANDN()) * rho1 * ((2 * x_delta[i] * x[i]) / (p[i] - q[i] + epsilon)) + (RAND_DOUBLE()) * rho2 * (x_best[i] - x[i]);
 
-		x2[i] = x_best[i] - randn() * rho1 * ((2 * x_delta[i] * x[i]) / (p[i] - q[i] + epsilon)) + rand_num() * rho2 * (xr1[i] - xr2[i]);
+		x2[i] = x_best[i] - (RANDN()) * rho1 * ((2 * x_delta[i] * x[i]) / (p[i] - q[i] + epsilon)) + (RAND_DOUBLE()) * rho2 * (xr1[i] - xr2[i]);
 
 		x3[i] = x[i] - rho1 * (x2[i] - x1[i]);
 
@@ -136,7 +126,7 @@ void leo(double x_next[22], double x[22], double x_best[22], double x1[22], doub
 	
 	//Вычисление Y
 	double y [22];
-	if (rand_num() < 0.5) {
+	if ((RAND_DOUBLE()) < 0.5) {
 		memcpy(y, x, 22);
 	}
 	else{
@@ -144,14 +134,14 @@ void leo(double x_next[22], double x[22], double x_best[22], double x1[22], doub
 	}
 
 	//Инициализация mu1
-	double mu1 = rand_num();
+	double mu1 = (RAND_DOUBLE());
 
 	//Вычисление u1, u2, u3
 	double u1, u2, u3;
 	if (mu1 < 0.5) {
-		u1 = 2 * rand_num();
-		u2 = rand_num();
-		u3 = rand_num();
+		u1 = 2 * (RAND_DOUBLE());
+		u2 = (RAND_DOUBLE());
+		u3 = (RAND_DOUBLE());
 	}
 	else {
 		u1 = 1.0;
@@ -160,7 +150,7 @@ void leo(double x_next[22], double x[22], double x_best[22], double x1[22], doub
 	}
 
 	//Инициализация mu2
-	double mu2 = rand_num();
+	double mu2 = (RAND_DOUBLE());
 
 	//Вычисление Xk
 	double x_k [22];
@@ -171,8 +161,8 @@ void leo(double x_next[22], double x[22], double x_best[22], double x1[22], doub
 		memcpy(x_p, x_rand, 22);
 	}
 
-	double f1 = -1.0 + 2.0 * rand_num(); //Инициализация f1
-	double f2 = -1.0 + 2.0 * rand_num(); //Инициализация f2
+	double f1 = -1.0 + 2.0 * (RAND_DOUBLE()); //Инициализация f1
+	double f2 = -1.0 + 2.0 * (RAND_DOUBLE()); //Инициализация f2
 
 	double rho1 = new_rho(alpha); //Инициализация rho1
 
@@ -206,7 +196,7 @@ void gbo(double population[][22], int m, int n, double pr, double th, double dct
 			double x_p[22];    //рандомный вектор p	
 			double x_rand[22]; //рандомно сгенерированный вектор
 			for (int i = 0; i < 22; i++) {
-				x_rand[i] = (double)(-th) + 2 * (double)th * rand_num();
+				x_rand[i] = (double)(-th) + 2 * (double)th * (RAND_DOUBLE());
 			}
 			double x_next[22];  //новый вектор
 
@@ -223,7 +213,7 @@ void gbo(double population[][22], int m, int n, double pr, double th, double dct
 			//Вычисление нового вектора через GSR
 			gsr(x_next, population[cur_vec], x1, x2, population[best_ind], population[worst_ind], xr1, xr2, xr3, xr4, cur_iter, m, n, &alpha);
 
-			if (rand_num() < pr) {
+			if ((RAND_DOUBLE())() < pr) {
 				//Вычисление нового вектора через LEO
 				leo(population[cur_vec], x_next, population[best_ind], x1, x2, xr1, xr2, x_p, x_rand, alpha, th);
 			}
